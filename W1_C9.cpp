63f3a705f205c7f276d38801e26fc7a00ae3f8aa
Devharsh Trivedi <devharsh@live.in>
2018-03-25 13:24:16 +0530

    Second commit

--- W1_C9.cpp
+++ W1_C9.cpp
@@ -20,17 +20,44 @@ int main()
 	ipfl.close();
 
 	float sum = 0;
-	int count = 0;
+	float used[15];
+	float unused[15];
+	int uc = 0, uuc = 0;
 
+	int count = 0;
 	for (int i = 0; i < n; i++)
 	{
 		sum += nary[i];
 		if (sum <= 300)
+		{
 			count++;
+			used[uc] = nary[i];
+			uc++;
+		}
 		else
+		{
 			sum -= nary[i];
+			unused[uuc] = nary[i];
+			uuc++;
+		}
 	}
 
+	int count2 = count;
+	for (int i = 0; i < uc; i++)
+	{
+		sum -= used[i];
+		count2--;
+		for (int j = 0; j < uuc; j++)
+		{
+			sum += unused[j];
+			if (sum <= 300)
+				count2++;
+			else
+				sum -= unused[j];
+		}
+	}
+
+	count = count > count2 ? count : count2;
 	std::ofstream opfl;
 	opfl.open("output.txt");
 	opfl << count;
